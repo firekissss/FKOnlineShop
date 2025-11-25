8275511ba68db917884a9c2edf96c6f0e45c17f7
firekissss <darkchrom74@gmail.com>
2025-11-25 13:36:53 +0300

    feat: add new tests, fix previous ones

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,34 +1,46 @@
 from src.category import Category
-from src.product import Product
 
 
-def test_category(category_1, category_2):
+# test initializing
+
+
+def test_category(category_1, category_2, product_1, product_2):
     assert isinstance(category_1, Category)
     assert category_1.name == "Смартфоны"
     assert (
         category_1.description
         == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
-    assert len(category_1.products) == 2
-
-    assert isinstance(category_1.products[0], Product)
-    assert isinstance(category_1.products[1], Product)
-
-    assert category_1.products[0].name == "Samsung Galaxy S23 Ultra"
-    assert category_1.products[1].description == "512GB, Gray space"
-
-    assert category_1.category_count
 
     assert isinstance(category_2, Category)
     assert category_2.name == "Телевизоры"
-    assert "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
-    assert len(category_2.products) == 1
+    assert (
+        category_2.description
+        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    )
 
-    assert isinstance(category_2.products[0], Product)
-    assert category_2.products[0].name == "Samsung Galaxy S23 Ultra"
 
+def test_category_adding_products(category_1, category_2, product_1, product_2):
+    # test products getter (empty for now)
+    assert isinstance(category_1.products, str)
+    assert category_1.products == ""
+
+    assert isinstance(category_2.products, str)
+    assert category_2.products == ""
+
+    # test add_products (almost products setter, but it's not)
+    category_1.add_product(product_1)
+    category_2.add_product(product_2)
+
+    # now there will be some products, checking them with getter
+    assert category_1.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert category_2.products == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+
+    # test class attributes
+    assert Category.category_count == 2
     assert category_1.category_count == 2
     assert category_2.category_count == 2
 
-    assert category_1.product_count == 3
-    assert category_2.product_count == 3
+    assert Category.product_count == 2
+    assert category_1.product_count == 2
+    assert category_2.product_count == 2
